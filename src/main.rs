0fbf161db12dbe820611e210e622bd8ff0970fe0
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-04-16 17:45:16 -0400

    src/main.rs:

--- src/main.rs
+++ src/main.rs
@@ -17,67 +17,120 @@ fn for_loop_1(args: Vec<String>) {
     }
 }
 fn system_calls(args: Vec<String>) {
+    if !args.is_empty() {
+        println!("system_calls:args={:?}", args);
+        //let last_arg = args.pop().unwrap();
+        //println!("Last argument: {}", last_arg);
+
+        if args.len() == 1 {
+            println!("args[0]={:?}",args[0]);
+            use std::process;
+            process::exit(0);
+        }
+        if args.len() == 2 {
+            println!("args[0]={:?}",args[0]);
+            println!("args[1]={:?}",args[1]);
+            use std::process;
+            process::exit(0);
+        }
+        if args.len() == 3 {
+            println!("args[0]={:?}",args[0]);
+            println!("args[1]={:?}",args[1]);
+            println!("args[2]={:?}",args[2]);
+            use std::process;
+            process::exit(0);
+        }
+        if args.len() == 4 {
+            println!("args[0]={:?}",args[0]);
+            println!("args[1]={:?}",args[1]);
+            println!("args[2]={:?}",args[2]);
+            println!("args[3]={:?}",args[3]);
+            use std::process;
+            process::exit(0);
+        }
+    } else {
+        println!("system_calls:No arguments provided.");
+        usage();
+    }
     if args.len() >= 2 {
         let first_arg = &args[1];
         println!("system_calls:First argument: {}", first_arg);
         if first_arg == "1" {
             for_loop_1(args.clone());
         }
+        use std::process;
+        process::exit(0);
     } else {
-        //println!("Please provide at least one argument.");
+        usage();
     }
     //for_loop_1(args);
 }
 
+fn usage() {
+    println!("Usage:");
+    println!("	rust-for-loops [OPTIONS] <int>");
+    println!("	\n");
+    println!("	rust-for-loops [OPTIONS] <int>");
+    use std::process;
+    process::exit(0);
+}
+
 fn main() {
-    let mut my_vec = vec![1, 2, 3, 4];
-    let removed_element = my_vec.remove(0);
-    println!("Removed element: {}", removed_element);
-    println!("Remaining vector: {:?}", my_vec);
+    //let mut my_vec = vec![1, 2, 3, 4];
+    //let removed_element = my_vec.remove(0);
+    //println!("Removed element: {}", removed_element);
+    //println!("Remaining vector: {:?}", my_vec);
 
     let mut args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    let first_element = args.remove(0);
-    println!("first_element={}", first_element);
-    println!("Remaining vector: {:?}", args);
+
+    //println!("{:?}", args);
+
+    let app_name = args.remove(0);
+
+    println!("app_name={}", app_name);
+
+    println!("Remaining args: {:?}", args);
+
     //let mut args = env::args().skip(1);
-    //println!("{}",args);
+    //println!("args={:?}",args);
 
-    //if !args.is_empty() {
-    //    let last_arg = args.pop().unwrap();
-    //    println!("Last argument: {}", last_arg);
-    //} else {
-    //    println!("No arguments provided.");
-    //}
-
-    // Access arguments by index (be cautious with potential out-of-bounds)
-    if args.len() >= 2 {
-        let first_arg = &args[1];
-        println!("First argument: {}", first_arg);
+    if !args.is_empty() {
+        //let last_arg = args.pop().unwrap();
+        //println!("Last argument: {}", last_arg);
     } else {
-        //println!("Please provide at least one argument.");
+        //println!("No arguments provided.");
     }
-
-    println!("rust-for-loops");
+    println!("args={:?}", args);
     system_calls(args.clone());
 
-    if args.len() >= 2 {
-        let mut args = env::args().skip(1); // Skip the program name
+    // Access arguments by index (be cautious with potential out-of-bounds)
+    //if args.len() >= 2 {
+    //    let first_arg = &args[1];
+    //    println!("First argument: {}", first_arg);
+    //} else {
+    //    //println!("Please provide at least one argument.");
+    //}
 
-        if let Some(last_arg) = args.next_back() {
-            println!("Last argument: {}", last_arg);
-        } else {
-            println!("No arguments provided.");
-        }
-        if let Some(last_arg) = args.next_back() {
-            println!("Last argument: {}", last_arg);
-        } else {
-            println!("No arguments provided.");
-        }
-        if let Some(last_arg) = args.next_back() {
-            println!("Last argument: {}", last_arg);
-        } else {
-            println!("No arguments provided.");
-        }
-    }
+    //println!("rust-for-loops ");
+    //system_calls(args.clone());
+
+    //if args.len() >= 2 {
+    //    let mut args = env::args().skip(1); // Skip the program name
+
+    //    if let Some(last_arg) = args.next_back() {
+    //        println!("Last argument: {}", last_arg);
+    //    } else {
+    //        println!("No arguments provided.");
+    //    }
+    //    if let Some(last_arg) = args.next_back() {
+    //        println!("Last argument: {}", last_arg);
+    //    } else {
+    //        println!("No arguments provided.");
+    //    }
+    //    if let Some(last_arg) = args.next_back() {
+    //        println!("Last argument: {}", last_arg);
+    //    } else {
+    //        println!("No arguments provided.");
+    //    }
+    //}
 }
